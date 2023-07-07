9f96921fb55e7814a26ed2c28ea9d2dd118869f6
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2023-07-07 18:25:39 +1000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyTaskMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyTaskMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
